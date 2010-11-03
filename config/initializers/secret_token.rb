7a6a68eccc155830cfb256a564563e3927570ae1
Americo Savinon <americos@gmail.com>
2010-11-03 13:43:51 -0400

    Initial commit of checkme Rails3 Application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Checkme::Application.config.secret_token = 'b683c45566b1fd1bae5adbc6655b6355e94995994d77fa37c965fc0d400d89839a35a7f39800fd295bc15344144e74cfc92309906ce474e94396b05660285805'
